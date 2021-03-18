6e4742c1f9f1d712638297161b8ab7d5671fdb02
wmanns <wmanns@freenet.de>
2021-03-19 00:25:46 +0100

    vs 1.62

---
+++ mqtt_DHT11_DHT22.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import paho.mqtt.client as mqtt
+
+def set_MQTT_broker(MQTT_broker_name, MQTT_Port, MQTT_last_will, wait_secs):
+    print('Connecting with MQTT: {0}'.format(MQTT_broker_name))
+    mqttc = mqtt.Client(MQTT_broker_name)
+    mqttc.will_set(MQTT_last_will, payload = 'offline', qos = 0, retain = True)
+    mqttc.connect (MQTT_broker_name, MQTT_Port, keepalive = wait_secs + 10)
+    mqttc.publish (MQTT_last_will, payload = 'online',  qos = 0, retain = True)
+    return mqttc
+
+def main():
+    mqttc = set_MQTT_broker(MQTT_broker_name = 'rh-rb-openhab',
+                            MQTT_Port = 1883,
+                            MQTT_last_will = MQTT_last_will,
+                            wait_secs= wait_secs)
+    return mqttc
+
+if __name__ == '__main__':
+    main()
+
